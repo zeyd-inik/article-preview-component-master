63a95df154c859c0394b1ac6d2694574a2357298
zeyd <1zeydinik@gmail/com>
2023-01-18 21:23:39 +0100

    create global styles

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
-<script setup>
-import TheWelcome from './components/TheWelcome.vue';
-</script>
+<script setup></script>
 
 <template>
-    <TheWelcome />
+    <TheWelcome></TheWelcome>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+/* Colors */
+$Very-Dark-Grayish-Blue: hsl(217, 19%, 35%);
+$Desaturated-Dark-Blue: hsl(214, 17%, 51%);
+$Grayish-Blue: hsl(212, 23%, 69%);
+$Light-Grayish-Blue: hsl(210, 46%, 95%);
+
+/* Custom Font */
+@import url('https://fonts.googleapis.com/css2?family=Manrope:wght@500;700&display=swap');
+
+/* Global Styles */
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Manrope', sans-serif;
+    font-size: 13px;
+}
+</style>
